8501a578243d8027954a181c9347cfe28661e352
Susy Esmeralda Díaz Bustillos <108904269+DBHRN@users.noreply.github.com>
2025-11-10 16:08:20 -0600

    Agregando url para peticiones a backend

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/dark-mode.css';
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
